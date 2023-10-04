a864d531c09ed3ba7fc5c96fc502ea151a513076
Searock Ruzario <searock@users.noreply.github.com>
2023-10-04 12:25:40 -0300

    updated to 2.0

---
+++ app/src/main/java/com/example/ticketmasterretailandticketsdemo/extension/Context.kt
@@ -0,0 +1,8 @@
+package com.example.ticketmasterretailandticketsdemo.extension
+
+import android.content.Context
+import android.view.View
+
+fun Context.hideKeyboard(view: View) {
+    view.hideKeyboard()
+}
